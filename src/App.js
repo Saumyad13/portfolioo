ff1c8c375489af3590472a5c0dc0478b0643cf71
Saumyad13 <104243590+Saumyad13@users.noreply.github.com>
2022-11-15 01:37:58 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import logo from './logo.svg';
+import logo1 from './img2022.jpeg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo1} className="App-logo" alt="logo1" />
+        <p>
+          <h1>Hi, I am Saumya Dwivedi</h1>
+          
+          
+        </p>
+        <p><h2>About me</h2>
+          I am pursuing Btech in Computer Science Engineering from SRM Institute of Science and Technology.
+          Currently I have been an active learner of coding and development.
+          Going forward I have also gained experience in C,C++,HTML,CSS, Java Script  and ReacTJS.</p>
+
+         <p> <h3>My work</h3>
+          Being an active participant in mini projects and aiming to be one in workshops and Hackathons forecoming.
+          So,at the moment  my main focus is on training,gaining skills and knowledge.</p>
+
+          <p><h4>My aim</h4>
+          My main aim as a developer during coding is to keep my codes sleek and always human-understandable.
+ 
+My daily task is to be equipped with the right knowledge and tools to enable me achieve these as I code.
+ </p>
+          
+         <p> <h4>Ways to connect</h4>
+         
+                  My linkedin handle:
+        <a
+          className="App-link"
+          href="https://www.linkedin.com/in/saumya-dwivedi-5619b0220?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3Br%2Fp8GFVjS0u9mVFcbON53w%3D%3D"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          LINKEDIN LINK
+        </a>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
